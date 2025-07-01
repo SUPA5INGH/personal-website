6050712a4a8c136bbd59ce0f5687757bfaba9421
Rohan Singh <rohan.s11400@gmail.com>
2025-07-01 19:29:13 +0100

    Add React imports for compatibility

--- README.md
+++ README.md
@@ -22,7 +22,6 @@ npm run dev
 
 Then open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-
 ## License
 
 This project is licensed under the MIT License. See [LICENSE](./LICENSE) for details.
---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+import Layout from '../components/Layout';
+
+export default function About() {
+  return (
+    <Layout>
+      <Head>
+        <title>About</title>
+      </Head>
+      <div className="min-h-screen p-8 bg-gray-900 text-gray-100">
+        <h1 className="text-3xl font-bold mb-4 text-gold">About</h1>
+        <p>A short biography will appear here.</p>
+      </div>
+    </Layout>
+  );
+}
