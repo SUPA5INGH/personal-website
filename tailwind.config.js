7a7c0aba6e2d97510b9626d051e38613be718f70
Rohan Singh <rohan.s11400@gmail.com>
2025-07-02 13:02:57 +0100

    Refine color themes and nav animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'pastel-blue': '#a7d8de',
         'pastel-yellow': '#fff3b0',
         'dark-green': '#386641',
+        'sage-100': '#E9F5DB',
+        'sage-300': '#CFE1B9',
+        'sage-500': '#97A97C',
+        'sage-700': '#718355',
+      },
+      keyframes: {
+        jiggle: {
+          '0%, 100%': { transform: 'translateX(var(--move)) scale(1)' },
+          '50%': { transform: 'translateX(var(--move)) scale(1.1)' },
+        },
+      },
+      animation: {
+        jiggle: 'jiggle 0.3s ease',
       },
     },
   },
