3ba90729e8c8f9447c5ec00a9cd4c304be1b3e54
Rohan Singh <rohan.s11400@gmail.com>
2025-07-02 17:42:53 +0100

    Add bento page transition system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
-
 import React from 'react';
 
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import BentoPageTransition from '../components/BentoPageTransition';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <BentoPageTransition>
+      <Component {...pageProps} />
+    </BentoPageTransition>
+  );
 }
