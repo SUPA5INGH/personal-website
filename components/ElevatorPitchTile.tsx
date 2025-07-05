c2333d56fd5f50683ca70a883d338154432b6c1b
Rohan Singh <rohan.s11400@gmail.com>
2025-07-05 22:34:12 +0100

    Update ElevatorPitchTile.tsx

--- components/ElevatorPitchTile.tsx
+++ components/ElevatorPitchTile.tsx
@@ -20,10 +20,10 @@ export default function ElevatorPitchTile() {
       aria-label="Personal introduction: Hi I'm Rohan, Med student turned tech-forward biosecurity thinker."
       className="col-span-full sm:col-span-2 md:col-span-2 row-span-2 rounded-2xl bg-white bg-cover shadow-lg hover:shadow-xl p-6"
     >
-      <h1 className="font-poppins font-bold text-2xl leading-10 text-white">
+      <h1 className="font-poppins font-bold text-2xl leading-10 text-black">
         Hi, I'm Rohan.
       </h1>
-      <p className="font-poppins font-medium text-xl leading-8 text-white">
+      <p className="font-poppins font-medium text-xl leading-8 text-black">
         <span>{text}</span>
         <span className="ml-1 font-mono border-r-2 border-current animate-pulse" />
       </p>
