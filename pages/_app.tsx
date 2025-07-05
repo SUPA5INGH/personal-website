0fbf8ac412bd11dfe4d8aaca9d95543ce52013ec
Rohan Singh <rohan.s11400@gmail.com>
2025-07-06 00:47:03 +0100

    Remove Bento page transition system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-import BentoPageTransition from '../components/BentoPageTransition';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <BentoPageTransition>
-      <Component {...pageProps} />
-    </BentoPageTransition>
-  );
+  return <Component {...pageProps} />;
 }
