1f14cccfb45b9a79ad9b5826b0668009fa045a36
Rohan Singh <rohan.s11400@gmail.com>
2025-07-06 13:16:39 +0100

    Update PolaroidSelfieTile.tsx

--- components/PolaroidSelfieTile.tsx
+++ components/PolaroidSelfieTile.tsx
@@ -56,7 +56,7 @@ export default function PolaroidSelfieTile({
           src="/images/IMG_8264.jpeg"
           alt="Rohan"
           fill
-          className="rounded-lg object-cover border-[12px] border-white pb-12"
+          className="rounded-lg object-cover border-[15px] border-white pb-15"
         />
         <motion.figcaption
           variants={captionVariants}
